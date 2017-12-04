6308c090ce49b29e31dea612b240facc26fc4e99
YifengGuo <gyf940517@gmail.com>
2017-12-04 15:18:13 -0500

    normalizer

---
+++ backpropagation/Normalizer.py
@@ -0,0 +1,17 @@
+# to check and correct the gradient of NN
+
+
+class Normalizer(object):
+    def __init__(self):
+        self.mask = [
+            0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80
+        ]
+
+    def norm(self, number):
+        return map(lambda m: 0.9 if number & m else 0.1, self.mask)
+
+    def denorm(self, vec):
+        binary = map(lambda i : 1 if i > 0.5 else 0, vec)
+        for i in range(len(self.mask)):
+            binary[i] = binary[i] * self.mask[i]
+        return reduce(lambda x, y: x + y, binary)
